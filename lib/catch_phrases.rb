fffb5186987db004a945d5010d461b4e33cd53f3
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-18 20:27:58 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,6 @@
+$status = "Thank You Mario! But Our Princess Is In Another Castle!"
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
@@ -8,3 +9,5 @@ def toadstool
   puts status
 end
 
+def link 
+  phrase = 
